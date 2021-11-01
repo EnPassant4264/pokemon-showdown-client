fd15995981420de744d21adb9874ad0137002f05
U-LAPTOP-QCSNC88L\pswan <peterswanton19@outlook.com>
2021-11-01 14:53:54 -0400

    hhh

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (8d8b63b1/55fadf43)";
+Config.version = "0.11.2 (de3af4d8/55fadf43)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
