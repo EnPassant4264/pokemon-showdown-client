efc79fddbfb5ee49b7ea2dcd5af608af44be1c26
En Passant <enpassant4264@gmail.com>
2022-02-23 01:40:35 -0700

    Revert "more data"
    This reverts commit fa907e37e4d86ca3c3c652fe5efa37b2dc97d1e5.

--- config/config.js
+++ config/config.js
@@ -33,7 +33,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2 (f1888ba5/55fadf43)";
+Config.version = "0.11.2 (857a3f6c/55fadf43)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
